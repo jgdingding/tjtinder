ff51edc565d89780a74075ac8487e90f646a9ed7
Matthew Chen <mchen2001@gmail.com>
2018-03-10 21:35:46 -0500

    idk

--- js/regisration.js
+++ js/regisration.js
@@ -1,17 +1,19 @@
-var mysql = require('mysql');
+function Register (){
+  var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "mysql1.csl.tjhsst.edu:3306",
-  user: "site_tjtinder",
-  password: "EktntpYTB7VYwPpeNHgtE4UN"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO Persons (FirstName, LastName, Email, Password) VALUES (document.getElementById("first_name").innerHTML, document.getElementById("last_name").innerHTML, document.getElementById("email").innerHTML, document.getElementById("password").innerHTML)"
-  con.query(sql, [VALUES], function(err, result)) {
-    if(err)throw err;
-    console.log("Registered");
+  var con = mysql.createConnection({
+    host: "mysql1.csl.tjhsst.edu:3306",
+    user: "site_tjtinder",
+    password: "EktntpYTB7VYwPpeNHgtE4UN"
   });
-});
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO Persons (FirstName, LastName, Email, Password) VALUES (document.getElementById("first_name").innerHTML, document.getElementById("last_name").innerHTML, document.getElementById("email").innerHTML, document.getElementById("password").innerHTML)"
+    con.query(sql, [VALUES], function(err, result)) {
+      if(err)throw err;
+      console.log("Registered");
+    });
+  });
+}
